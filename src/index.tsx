03c668e524d54bd5ef877c4c0c718f5ccbff1e96
Dmitriy <shalberkind@mail.ru>
2021-04-29 22:29:46 +0300

    Project setup

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <>
+      <h1>Application</h1>
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
