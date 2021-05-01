79bb44f93b5f5a94801bcea8350f3d496697b24f
Dmitriy <shalberkind@mail.ru>
2021-05-01 23:08:45 +0300

    Bundling user input

--- src/plugins/unpkge-path-plugin.ts
+++ src/plugins/unpkge-path-plugin.ts
@@ -13,7 +13,7 @@ const fileCache = localForage.createInstance({
   console.log(color);
 })();
 
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
@@ -46,11 +46,7 @@ export const unpkgPathPlugin = () => {
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              import React, {useState} from 'react';
-              
-              console.log('React:',React);
-            `,
+            contents: inputCode,
           };
         }
 
