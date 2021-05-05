fa923d12c1a428ef4bccde5c1c96f82f39081443
Dmitriy <shalberkind@mail.ru>
2021-05-05 23:56:37 +0300

    Adding a type definition

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,4 +1,4 @@
-import MonacoEditor from '@monaco-editor/react';
+import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
 
 interface CodeEditorProps {
   initialValue: string;
@@ -9,10 +9,12 @@ export const CodeEditor: React.FC<CodeEditorProps> = ({
   onChange,
   initialValue,
 }) => {
-  const onEditorDidMount = (getValue: () => string, monacoEditor: any) => {
+  const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
     monacoEditor.onDidChangeModelContent(() => {
       onChange(getValue());
     });
+
+    monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
   };
 
   return (
