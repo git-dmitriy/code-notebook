d2e72fa1278d51e3da880d4f04be059c0a03fb3f
Dmitriy <shalberkind@mail.ru>
2021-05-07 12:31:48 +0300

    Extract the code cell component

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,11 @@
 import ReactDOM from 'react-dom';
-import { useState } from 'react';
-import bundle from './bundler';
-
-import { CodeEditor } from './components/code-editor';
-import { Preview } from './components/preview';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import { CodeCell } from './components/code-cell';
 
 const App = () => {
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
-
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
-
   return (
     <>
-      <CodeEditor initialValue='' onChange={(value) => setInput(value)} />
-      <div>
-        <button onClick={onClick}>submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
     </>
   );
 };
