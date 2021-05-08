495170c98f6adbb521287f4a8b6fd0a749ec9f7a
Dmitriy <shalberkind@mail.ru>
2021-05-08 23:54:37 +0300

    Adding an execution timeout
    To fix the disappearing preview

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -30,7 +30,10 @@ export const Preview: React.FC<PreviewProps> = ({ code }) => {
   const iframe = useRef<any>();
   useEffect(() => {
     iframe.current.srcdoc = html;
-    iframe.current.contentWindow.postMessage(code, '*');
+
+    setTimeout(() => {
+      iframe.current.contentWindow.postMessage(code, '*');
+    }, 50);
   }, [code]);
 
   return (
