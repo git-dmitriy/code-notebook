21d86a80706b1e5aeb25c479fa5c84dd9a533a14
Dmitriy <shalberkind@mail.ru>
2021-05-08 23:10:42 +0300

    Manage editor width during page resize

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,4 +1,5 @@
 import { ResizableBox, ResizableBoxProps } from 'react-resizable';
+import { useEffect, useState } from 'react';
 import './resizeable.css';
 
 interface ResizableProps {
@@ -9,23 +10,52 @@ export const Resizable: React.FC<ResizableProps> = ({
   direction,
   children,
 }) => {
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+  const [width, setWidth] = useState(window.innerWidth * 0.75);
+
+  useEffect(() => {
+    let timer: any;
+    const listener = () => {
+      if (timer) {
+        clearInterval(timer);
+      }
+      timer = setTimeout(() => {
+        setInnerWidth(window.innerWidth);
+        setInnerHeight(window.innerHeight);
+        if (window.innerWidth < width) {
+          setWidth(window.innerWidth * 0.75);
+        }
+      }, 100);
+    };
+
+    window.addEventListener('resize', listener);
+
+    return () => {
+      window.removeEventListener('resize', listener);
+    };
+  }, []);
+
   let resizableProps: ResizableBoxProps;
 
   if (direction === 'horizontal') {
     resizableProps = {
       className: 'resize-horizontal',
-      width: window.innerWidth * 0.75,
+      width,
       height: Infinity,
       resizeHandles: ['e'],
-      maxConstraints: [window.innerWidth * 0.75, Infinity],
-      minConstraints: [window.innerWidth * 0.2, Infinity],
+      maxConstraints: [innerWidth * 0.75, Infinity],
+      minConstraints: [innerWidth * 0.2, Infinity],
+      onResizeStop: (event, data) => {
+        setWidth(data.size.width);
+      },
     };
   } else {
     resizableProps = {
       width: Infinity,
       height: 300,
       resizeHandles: ['s'],
-      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      maxConstraints: [Infinity, innerHeight * 0.9],
       minConstraints: [Infinity, 35],
     };
   }
