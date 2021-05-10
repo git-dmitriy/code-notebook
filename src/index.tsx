830072c55c2ac057dc535b0b7c490b2191417b85
Dmitriy <shalberkind@mail.ru>
2021-05-10 19:56:54 +0300

    Toggling the markdown editor mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import ReactDOM from 'react-dom';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import { CodeCell } from './components/code-cell';
+import { TextEditor } from './components/text-editor';
 
 const App = () => {
   return (
     <>
-      <CodeCell />
+      <TextEditor />
     </>
   );
 };
