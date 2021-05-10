1d06b14c38e928da9773a5f82d6ed32503feb46b
Dmitriy <shalberkind@mail.ru>
2021-05-10 23:08:28 +0300

    Styling preview window

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -40,11 +40,13 @@ export const TextEditor: React.FC = () => {
 
   return (
     <div
-      className='text-editor'
+      className='text-editor card'
       onClick={() => {
         setEditing(true);
       }}>
-      <MDEditor.Markdown source={value} />
+      <div className='card-content'>
+        <MDEditor.Markdown source={value} />
+      </div>
     </div>
   );
 };
