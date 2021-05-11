d8f52b1cc7ab22d3e068e94699c47976d731737f
Dmitriy <shalberkind@mail.ru>
2021-05-11 23:54:50 +0300

    Action creator implementations

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -6,11 +6,44 @@ import {
   MoveCell,
   InsertCellBefore,
 } from '../actions';
+import { CellType } from '../cell';
 
-export const updateCell = (): UpdateCell => {};
+export const updateCell = (id: string, content: string): UpdateCell => {
+  return {
+    type: ActionType.UPDATE_CELL,
+    payload: {
+      id,
+      content,
+    },
+  };
+};
 
-export const deleteCell = (): DeleteCell => {};
+export const deleteCell = (id: string): DeleteCell => {
+  return {
+    type: ActionType.DELETE_CELL,
+    payload: id,
+  };
+};
 
-export const moveCell = (): MoveCell => {};
+export const moveCell = (id: string, direction: 'up' | 'down'): MoveCell => {
+  return {
+    type: ActionType.MOVE_CELL,
+    payload: {
+      id,
+      direction,
+    },
+  };
+};
 
-export const insertCellBefore = (): InsertCellBefore => {};
+export const insertCellBefore = (
+  id: string,
+  cellType: CellType
+): InsertCellBefore => {
+  return {
+    type: ActionType.INSERT_CELL_BEFORE,
+    payload: {
+      id,
+      type: cellType,
+    },
+  };
+};
