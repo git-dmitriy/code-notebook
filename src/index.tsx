6534f21dc4461809329240f99b55c5a70cb7fe1f
Dmitriy <shalberkind@mail.ru>
2021-05-13 23:03:55 +0300

    Create cellList component

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './state';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import { TextEditor } from './components/text-editor';
+import { CellList } from './components/cell-list';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <TextEditor />
+      <CellList />
     </Provider>
   );
 };
