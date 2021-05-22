dd3ef2081cde39a03c086975d80051cc3bb35185
Dmitriy <shalberkind@mail.ru>
2021-05-22 14:01:57 +0300

    Create custom styles for action-bar

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,6 +1,8 @@
 import { Cell } from '../state';
 import { CodeCell } from './code-cell';
 import { TextEditor } from './text-editor';
+import ActionBar from './action-bar';
+import './cell-list-item.css';
 
 interface CellListItemProps {
   cell: Cell;
@@ -9,10 +11,24 @@ interface CellListItemProps {
 export const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
   let child: JSX.Element;
   if (cell.type === 'code') {
-    child = <CodeCell cell={cell} />;
+    child = (
+      <>
+        <div className='action-bar-code-wrapper'>
+          <ActionBar id={cell.id} />
+        </div>
+        <CodeCell cell={cell} />
+      </>
+    );
   } else {
-    child = <TextEditor cell={cell} />;
+    child = (
+      <>
+        <div className='action-bar-text-wrapper'>
+          <ActionBar id={cell.id} />
+        </div>
+        <TextEditor cell={cell} />
+      </>
+    );
   }
 
-  return <div>{child}</div>;
+  return <div className='cell-list-item'>{child}</div>;
 };
