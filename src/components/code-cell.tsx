02297b4bb5d1c294d9e16b917bcc5cf849233c68
Dmitriy <shalberkind@mail.ru>
2021-06-05 19:20:50 +0300

    Create show function for objects and jsx

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -19,7 +19,24 @@ export const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const { data, order } = state.cells;
     const orderedCell = order.map((id) => data[id]);
 
-    const cumulativeCode = [];
+    const cumulativeCode = [
+      `
+      import _React from 'react';
+      import _ReactDOM from 'react-dom';
+      const show = (value) => {
+        const root = document.querySelector('#root');
+        if (typeof value === 'object') {
+          if (value.$$typeof && value.props) {
+            _ReactDOM.render(value, root);
+          } else {
+            root.innerHTML = JSON.stringify(value);
+          }
+        } else {
+          root.innerHTML = value;
+        }
+      };
+      `,
+    ];
     for (let c of orderedCell) {
       if (c.type === 'code') {
         cumulativeCode.push(c.content);
