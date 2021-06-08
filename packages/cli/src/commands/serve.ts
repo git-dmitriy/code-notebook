ae73469e8c3d1457015c29142b6d01599e53e595
Dmitriy <shalberkind@mail.ru>
2021-06-08 23:59:24 +0300

    Run express server

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -6,7 +6,11 @@ export const serveCommand = new Command()
   .command('serve [filename]')
   .description('Open file for editing')
   .option('-p, --port <number>', 'port to run server on', '4005')
-  .action((filename = 'notebook.js', options: { port: string }) => {
-    const dir = path.join(process.cwd(), path.dirname(filename));
-    serve(parseInt(options.port), path.basename(filename), dir);
+  .action(async (filename = 'notebook.js', options: { port: string }) => {
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename));
+      await serve(parseInt(options.port), path.basename(filename), dir);
+    } catch (err) {
+      console.log('Have a problem:', err.message);
+    }
   });
