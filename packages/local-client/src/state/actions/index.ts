15d0b78ced2a6560f022ae4d8bd1c89a8a731902
Dmitriy <shalberkind@mail.ru>
2021-06-11 16:38:04 +0300

    Create action types

--- packages/local-client/src/state/actions/index.ts
+++ packages/local-client/src/state/actions/index.ts
@@ -1,5 +1,5 @@
 import { ActionType } from '../action-types';
-import { CellType } from '../cell';
+import { Cell, CellType } from '../cell';
 
 export type Direction = 'up' | 'down';
 
@@ -50,10 +50,25 @@ export interface BundleCompleteAction {
   };
 }
 
+export interface FetchCellsAction {
+  type: ActionType.FETCH_CELLS;
+}
+export interface FetchCellsCompleteAction {
+  type: ActionType.FETCH_CELLS_COMPLETE;
+  payload: Cell[];
+}
+export interface FetchCellsErrorAction {
+  type: ActionType.FETCH_CELLS_ERROR;
+  payload: string;
+}
+
 export type Action =
   | MoveCell
   | DeleteCell
   | InsertCellAfter
   | UpdateCell
   | BundleStartAction
-  | BundleCompleteAction;
+  | BundleCompleteAction
+  | FetchCellsAction
+  | FetchCellsCompleteAction
+  | FetchCellsErrorAction;
